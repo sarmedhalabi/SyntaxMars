92e8cb5540a844a18a104d9314c42567b727b392
sarmedhalabi <sarmedhalabi@gmail.com>
2020-03-18 00:30:34 -0400

    updated file

--- src/Test1.java
+++ src/Test1.java
@@ -1,6 +1,9 @@
 
 public class Test1 {
 public static void main(String[] args) {
+	String name= " I love Java";
+	System.out.println(name.substring(2));
+	System.out.println(name.subSequence((1), 6));
 	
 }
 }
