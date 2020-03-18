792cfe417ae5232972c2abf98f16a7b1552382e7
sarmedhalabi <sarmedhalabi@gmail.com>
2020-03-17 21:14:27 -0400

    if statment added to class day02

--- src/com/class02/Day2.java
+++ src/com/class02/Day2.java
@@ -4,6 +4,10 @@ public class Day2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		boolean isRaining=true;
+		if (isRaining) {
+			System.out.println(" it is raining");
+		}
 
 	}
 
